648583f8af18d46d982abc13ae672b98ef8939c3
jdegene <j-f-d@gmx.de>
2016-07-01 13:24:42 +0200

    tactical lineup routine implemented -> though very very slow

--- Stats.py
+++ Stats.py
@@ -2,6 +2,7 @@
 
 import sqlite3
 import pandas as pd 
+import collections
 
 season = '2015'
 league = '1'
@@ -19,7 +20,14 @@ league2Teams = [ '1. FC Nürnberg', 'Fortuna Düsseldorf', 'Arminia Bielefeld', 
  'FC St. Pauli', 'VfL Bochum', 'Eintracht Braunschweig', 'FSV Frankfurt', 'Karlsruher SC',
  'SpVgg Greuther Fürth', '1. FC Heidenheim', 'RasenBallsport Leipzig', '1. FC Kaiserslautern']
  
- 
+
+##########################################################################################
+# Small handy fucntions
+##########################################################################################
+    
+# flatten any iterable into 1D
+def flatten(myList):
+    return [item for sublist in myList for item in sublist] 
  
 ##########################################################################################
 # DB/Pandas data extraction, creates a list of player cost,points
@@ -32,13 +40,15 @@ dfPlayer = pd.read_sql_query("SELECT * from Player", conDB)
 dfPlyStats = pd.read_sql_query("SELECT * from PlayerStats", conDB)
 
 # Load Points into dataframe
-dfBL = pd.read_sql_query("SELECT * from BL1_15", conDB)
+#dfBL = pd.read_sql_query("SELECT * from BL1_15", conDB)
+
+# calculate the sums of columns 1:35 (i.e. all gamedays) add it to new column 'Sum', sort by 'Sum' descending
+#dfBL['Sums'] = dfBL.iloc[:,1:35].sum(axis=1)
+#dfBLsorted = dfBL.sort_values("Sums", ascending=False)
 
 # Load Tactics into dataframe THIS TAKES TOO LONG, DATAFRAME WILL BE HUGE, RATHER USE SQLITE FOR THIS
 #dfTactics = pd.read_sql_query("SELECT * from Tactics1_15", conDB)
 
-# calculate the sums of columns 1:35 (i.e. all gamedays) add it to new column 'Sum', sort by 'Sum' descending
-dfBL['Sum'] = dfBL.iloc[:,1:35].sum(axis=1).sort("Sums", ascending=False)
 
 
 # points per player by grouping and summing, as_index prevents using ID as index (needed for join)
@@ -51,10 +61,23 @@ pSmerge = pd.merge(dfPlayer, pSums, on='Player_ID', how='inner')
 pL1 = pSmerge[pSmerge['Team'].isin(league1Teams)]
 pL2 = pSmerge[pSmerge['Team'].isin(league2Teams)]
 
-# create a dataframe with total points of each Manager
+# create a dataframe with total points of each Manager (oops, redundant, dfBL['Sums'] above does the same)
 dfPoints = pd.read_sql_query("SELECT * from BL{}_{}".format(league,season[2:]), conDB)
 col_list= list(dfPoints)[1:] # list of column names, without ID column
-dfPoints['Total'] = dfPoints[col_list].sum(axis=1) 
+dfPoints['Total'] = dfPoints[col_list].sum(axis=1)
+
+
+# add a column to dfBL with a collections.Counter of tactical lineup, and a column with the number of different lineups
+# WAY TOO SLOW, ONLY RUN WHEN REALLY NEEDED. THE VERSION WITHOUT SQLite needs the dfTactics dataframe
+#for x in dfPoints["Manager_ID"]:
+#    #DBoutput = c.execute('SELECT TacID FROM Tactics1_15 WHERE Manager_ID={}'.format(x)).fetchall()
+#    DFoutput = dfTactics[dfTactics["Manager_ID"]==4540209]["TacID"]
+#    #flatOutput = flatten(DBoutput)
+#    dfPoints["Counter"] = 0
+#    dfPoints["TacNum"] = 0
+#    dfPoints["Counter"].loc[dfPoints["Manager_ID"] == x] = dict(collections.Counter(DFoutput))
+#    dfPoints["TacNum"].loc[dfPoints["Manager_ID"] == x] = len(collections.Counter(DFoutput))
+
 
 
 # drop all columns not needed for knapsack and convert df to list
@@ -88,8 +111,6 @@ def managerSlice(upperBound, lowerBound):
     returndf = dfPoints.sort_values(by="Total")._slice(slice(toprow,bottomrow))
     
     return returndf
-    
-
 
 class Stats:
     
@@ -119,20 +140,21 @@ class Stats:
         
         return returnList 
     
-    # returns the average points for each tactical lineup SLOW
+    # returns the average points for each tactical lineup VERY VERY SLOW
     def pointsPerTactic():
         returnList = []
         for x in range(5):
             DBOutput = c.execute('SELECT Manager_ID, GameDay FROM Tactics1_15 WHERE TacID={}'.format(x)).fetchall()
             pointsList = []
             for y in DBOutput:
-                points = dfBL[dfBL["Manager_ID"]==y[0]]["GD"+str(y[1])].values[0]
+                points = dfPoints[dfPoints["Manager_ID"]==y[0]]["GD"+str(y[1])].values[0]
                 pointsList.append(int(points))
             avgPoints = sum(pointsList)/len(pointsList)
             returnList.append(avgPoints)
             
         return returnList
-            
+    
+        
 
             
     
